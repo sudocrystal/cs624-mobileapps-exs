1c472a7b94adbd82c869255d9766a4400ec5fb16
Sam Chung <chungsam@cityu.edu>
2023-05-06 17:17:05 +0000

    CS 624

--- Module07/CitiesApp/src/components/CenterMessage.js
+++ Module07/CitiesApp/src/components/CenterMessage.js
@@ -1,9 +1,4 @@
-import React from 'react'
-import {
-  Text,
-  View,
-  StyleSheet
-} from 'react-native'
+import { Text, View, StyleSheet } from 'react-native'
 
 import { colors } from '../theme'
 
